c87dacd58ef4eabdfaa00dc4baa6c455f714bd00
110619045 <43201134+110619045@users.noreply.github.com>
2022-07-02 19:44:16 +0800

    Add files via upload

---
+++ OpenHCI/js/main.js
@@ -0,0 +1,11 @@
+function click_card_open() {
+    document.getElementById('card_a_content').style.display='block';
+    document.getElementById('card_a_btn_close').style.display='block';
+    document.getElementById('card_a_btn_open').style.display='none';
+}
+
+function click_card_close() {
+    document.getElementById('card_a_content').style.display='none';
+    document.getElementById('card_a_btn_close').style.display='none';
+    document.getElementById('card_a_btn_open').style.display='block';
+}
